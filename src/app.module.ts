6fa4005622813d8450b7036c76b102c33c009fd4
nejcokorn <nejc.okorn@gmail.com>
2026-01-30 09:48:17 +0100

    Remove Hello endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from "@nestjs/common";
 import { CanModule } from './can/can.module';
 import { DeviceModule } from './device/device.module';
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { DeviceController } from "./device/device.controller";
 
 @Module({
 	imports: [CanModule, DeviceModule],
-	controllers: [AppController, DeviceController],
-	providers: [AppService],
+	controllers: [DeviceController],
+	providers: [],
 })
 export class AppModule {}
